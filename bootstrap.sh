424e4e852a3db623e896f15b659bf7ba974afb69
David Hisel <david.hisel@gmail.com>
2018-06-25 12:57:07 -0700

    removing bootstrap.sh script

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker-compose up -d --build
-docker-compose run --rm composer install
-docker-compose run --rm --entrypoint php  -w /var/www/html/app php bin/phinx migrate
-
-cd web/app
-./bin/codecept run
-
